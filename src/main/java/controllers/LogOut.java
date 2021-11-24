d2b7a3ac4cfd0bcb3360183e7a0f8579294b4bef
Juri Sacchetta <jurisacchetta@gmail.com>
2021-11-24 14:32:31 +0100

    Implemented LogOut

---
+++ src/main/java/controllers/LogOut.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Logout", value = "/Logout")
+public class LogOut extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.getSession().invalidate();
+        response.sendRedirect("index.html");
+    }
+
+}
