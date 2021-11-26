886239763f968c52d82c82a227a366bb27b4aa78
Giuseppe Tortorelli <peppetort@gmail.com>
2021-11-26 17:16:18 +0100

    getHomeServlet and customer home page template

--- src/main/java/entities/Offer.java
+++ src/main/java/entities/Offer.java
@@ -56,6 +56,10 @@ public class Offer implements Serializable {
         this.isActive = isActive;
     }
 
+    public ServicePackage getServicePackage(){
+        return servicePackage;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
