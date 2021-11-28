ac0c0f2bc3ac369d24446d06174bd8248462b0f0
Juri Sacchetta <jurisacchetta@gmail.com>
2021-11-28 12:06:26 +0100

    Create/customize order sevlets and HTML pages implemented

--- src/main/java/controllers/ReviewOrder.java
+++ src/main/java/controllers/ReviewOrder.java
@@ -1,28 +1,26 @@
 package controllers;
 
-import entities.Offer;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
-import services.PackageService;
+import services.BuyService;
 
-import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(name = "GetCustomerHome", value = "")
-public class GetCustomerHome extends HttpServlet {
+@WebServlet(name = "ReviewOrder", urlPatterns = "/ReviewOrder")
+public class ReviewOrder extends HttpServlet {
 
     private final TemplateEngine templateEngine = new TemplateEngine();
 
-    @EJB(beanName = "PackageService")
-    PackageService packageService;
+    @Inject
+    BuyService buyService;
 
     @Override
     public void init() {
@@ -35,16 +33,13 @@ public class GetCustomerHome extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        List<Offer> offerList = packageService.getAvailableOffer();
-
-        String path = "/WEB-INF/templates/CustomerHomePage.html";
+        String path = "/WEB-INF/templates/ReviewOrderPage";
         final WebContext ctx = new WebContext(request, response, getServletContext(), request.getLocale());
-        ctx.setVariable("offerList", offerList);
+        ctx.setVariable("order", buyService.getOrder());
         templateEngine.process(path, ctx, response.getWriter());
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        doGet(request, response);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
     }
 }
--- src/main/java/services/BuyService.java
+++ src/main/java/services/BuyService.java
@@ -2,52 +2,96 @@ package services;
 
 import entities.Offer;
 import entities.OptionalProduct;
+import entities.Order;
 
 import javax.ejb.Remove;
 import javax.ejb.Stateful;
+import javax.enterprise.context.SessionScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.ws.rs.BadRequestException;
-import java.util.HashSet;
-import java.util.Set;
+import javax.persistence.PersistenceContextType;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
+/**
+ * Http Session is a perfect storage place where all request from same web client have access
+ * to all attributes stored in http session. So you ask for it from the container and keep it in a place (http session)
+ * from where it can be referred again by all requests for same session.
+ * Therefore, if in the session there's no attribute userQuestionnaire, it means that it's the first time the user is fulfilling
+ * the product's questionnaire
+ * -> @Stateful EJBs can be also denoted with @SessionScoped annotation.
+ * Then the active HTTP session becomes the "EJB client" and maintains the instances.
+ */
+@SessionScoped
 @Stateful
-public class BuyService {
-    @PersistenceContext(unitName = "db2_project")
+public class BuyService implements Serializable {
+    private final Map<OptionalProduct, Boolean> optionalProductBooleanMap = new HashMap<>();
+    @PersistenceContext(unitName = "db2_project", type = PersistenceContextType.EXTENDED)
     private EntityManager em;
-    private Offer offer = null;
-    private final Set<OptionalProduct> optionalProductSet = new HashSet<>();
+    private Order order;
 
-    public Offer getOffer() {
-        return offer;
-    }
-
-    public void setOffer(int id) {
+    public void setOffer(int id) throws IllegalAccessException {
         Offer se = em.find(Offer.class, id);
-        if (offer != null || !se.isActive()) throw new BadRequestException();
-        offer = se;
+        if (se == null || !se.isActive()) throw new IllegalAccessException();
+        if (se.equals(order.getOffer())) return;
+        order.setOffer(se);
+        optionalProductBooleanMap.clear();
+        for (OptionalProduct o : order.getOffer().getServicePackage().getOptionalProductList()) {
+            optionalProductBooleanMap.put(o, Boolean.FALSE);
+        }
     }
 
-    public Set<OptionalProduct> getOptionalProductSet() {
-        return optionalProductSet;
+    public Map<OptionalProduct, Boolean> getOptionalProduct() {
+        return optionalProductBooleanMap;
     }
 
-    public void addOptionalProduct(OptionalProduct op) {
-        if (offer == null) throw new BadRequestException();
-        if (!offer.getServicePackage().getOptionalProductList().contains(op)) throw new BadRequestException();
-        optionalProductSet.add(op);
+    public void addOptionalProduct(int idOpProd) throws IllegalAccessException {
+        Optional<OptionalProduct> optional = optionalProductBooleanMap.keySet().stream().filter(x -> x.getId() == idOpProd).findAny();
+        if (optional.isEmpty()) throw new IllegalAccessException();
+        OptionalProduct o = optional.get();
+        optionalProductBooleanMap.replace(o, Boolean.TRUE);
+        order.getOptionalProductList().add(o);
+    }
+
+    public void setStartDate(Date date) throws IllegalAccessException {
+        if (new Date().before(date)) order.setStartDate(date);
+        else throw new IllegalAccessException();
     }
 
     public void removeItem(OptionalProduct op) {
-        optionalProductSet.remove(op);
+        optionalProductBooleanMap.replace(op, Boolean.FALSE);
+    }
+
+    private double evaluateMonthlyFee() {
+        if (order.getOffer() == null) return 0;
+        double sum = 0;
+        sum = sum + order.getOffer().getMonthlyFee();
+        for (Map.Entry<OptionalProduct, Boolean> e : optionalProductBooleanMap.entrySet()) {
+            if (e.getValue()) sum = sum + e.getKey().getMonthlyFee();
+        }
+        return sum;
+    }
+
+    public Order getOrder() {
+        if(order != null) order.setTotalMonthlyFee(evaluateMonthlyFee());
+        return order;
+    }
+
+    public boolean isCorrectFilled() {
+        return order.isCorrectFilled();
+    }
+
+    public void init() {
+        order = new Order();
+        order.setCreationDate(new Date());
+        order.setTotalMonthlyFee(evaluateMonthlyFee());
     }
 
     @Remove
-    public void checkout(int paymentId) {
-        //TODO payment method's implementation
-    }
-
-    @Remove
-    public void cancel() {
+    public void checkout() {
+        //TODO incoming...
     }
 }
