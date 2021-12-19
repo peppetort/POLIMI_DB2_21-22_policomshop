8745e1e981534dca6601cdd4f3685e1074cb0158
Juri Sacchetta <jurisacchetta@gmail.com>
2021-12-19 11:30:12 +0100

    First attempt

---
+++ EJB/src/it.polimi.DB2/java/entities/AuditCustomer.java
@@ -0,0 +1,31 @@
+package entities;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "audit_customer", schema = "db2_project")
+@DiscriminatorValue("1")
+public class AuditCustomer extends Customer {
+    @Column(name = "amount")
+    private double amount;
+    @Column(name = "last_rejection")
+    private Timestamp lastRejection;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Timestamp getLastRejection() {
+        return lastRejection;
+    }
+
+    public void setLastRejection(Timestamp lastRejection) {
+        this.lastRejection = lastRejection;
+    }
+
+}
---
+++ WEB/src/it.polimi.DB2/java/controllers/Payment.java
@@ -0,0 +1,37 @@
+package controllers;
+
+import entities.Customer;
+import org.thymeleaf.context.WebContext;
+import services.BuyService;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.BadRequestException;
+import java.io.IOException;
+
+@WebServlet(name = "Payment", urlPatterns = "/Payment")
+public class Payment extends HttpServletThymeleaf {
+    @Inject
+    BuyService buyService;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        boolean flag;
+        try {
+            if (buyService.getOrder().getCustomer() == null)
+                buyService.getOrder().setCustomer((Customer) request.getSession().getAttribute("user"));
+            flag = buyService.executePayment();
+        } catch (BadRequestException e) {
+            e.getStackTrace();
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        String path = "PaymentPage";
+        final WebContext ctx = new WebContext(request, response, getServletContext(), request.getLocale());
+        ctx.setVariable("user", request.getSession().getAttribute("user"));
+        ctx.setVariable("result", flag);
+        templateEngine.process(path, ctx, response.getWriter());
+    }
+}
