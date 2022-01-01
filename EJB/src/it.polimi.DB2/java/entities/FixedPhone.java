84bbdc2a5aad8cc7ce2ea8077ceca6aaaef3c8b4
Juri Sacchetta <jurisacchetta@gmail.com>
2022-01-01 12:00:30 +0100

    Added table tag

--- EJB/src/it.polimi.DB2/java/entities/FixedPhone.java
+++ EJB/src/it.polimi.DB2/java/entities/FixedPhone.java
@@ -2,8 +2,10 @@ package entities;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "service", schema = "db2_project")
 @DiscriminatorValue("2")
 public class FixedPhone extends Service {
 
