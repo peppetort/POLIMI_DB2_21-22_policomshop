3562a9ff8b40162503057e375c24a6f745e8d654
Giuseppe Tortorelli <peppetort@gmail.com>
2022-01-15 12:39:28 +0100

    report servlet restored

--- EJB/src/it.polimi.DB2/java/services/ReportService.java
+++ EJB/src/it.polimi.DB2/java/services/ReportService.java
@@ -17,37 +17,38 @@ public class ReportService {
     @PersistenceContext(unitName = "db2_project")
     EntityManager em;
 
-    public Map<ServicePackage, Long> getTotalPurchasesPerPackage() {
-        try {
-            Map<ServicePackage, Long> stat = new HashMap<>();
-            List<ServicePackage> servicePackageList = em.createQuery("select sp from ServicePackage sp", ServicePackage.class).getResultList();
+//    public Map<ServicePackage, Long> getTotalPurchasesPerPackage() {
+//        try {
+//            Map<ServicePackage, Long> stat = new HashMap<>();
+//            List<ServicePackage> servicePackageList = em.createQuery("select sp from ServicePackage sp", ServicePackage.class).getResultList();
+//
+//            for (ServicePackage sp : servicePackageList) {
+//                Long numberOfPurchasedPackage = em.createQuery("select count(a) from ActivationSchedule a where a.servicePackage.id = :servicePackageId", Long.class).setParameter("servicePackageId", sp.getId()).getSingleResult();
+//                stat.put(sp, numberOfPurchasedPackage);
+//            }
+//
+//            return stat;
+//        } catch (PersistenceException e) {
+//            throw new BadRequestException();
+//        }
+//    }
+//
+//    public Map<Offer, Long> getTotalPurchasePerPackageAndValidityPeriod() {
+//        try {
+//            Map<Offer, Long> stat = new HashMap<>();
+//            List<Offer> offerList = em.createQuery("select o from Offer o order by o.servicePackage.id", Offer.class).getResultList();
+//
+//            for (Offer o : offerList) {
+//                Long numberOfPurchasedOffer = em.createQuery("select count(a) from ActivationSchedule a where a.offer.id = :offerId ", Long.class).setParameter("offerId", o.getId()).getSingleResult();
+//                stat.put(o, numberOfPurchasedOffer);
+//            }
+//
+//            return stat;
+//        } catch (PersistenceException e) {
+//            throw new BadRequestException();
+//        }
+//    }
 
-            for (ServicePackage sp : servicePackageList) {
-                Long numberOfPurchasedPackage = em.createQuery("select count(a) from ActivationSchedule a where a.servicePackage.id = :servicePackageId", Long.class).setParameter("servicePackageId", sp.getId()).getSingleResult();
-                stat.put(sp, numberOfPurchasedPackage);
-            }
-
-            return stat;
-        } catch (PersistenceException e) {
-            throw new BadRequestException();
-        }
-    }
-
-    public Map<Offer, Long> getTotalPurchasePerPackageAndValidityPeriod() {
-        try {
-            Map<Offer, Long> stat = new HashMap<>();
-            List<Offer> offerList = em.createQuery("select o from Offer o order by o.servicePackage.id", Offer.class).getResultList();
-
-            for (Offer o : offerList) {
-                Long numberOfPurchasedOffer = em.createQuery("select count(a) from ActivationSchedule a where a.offer.id = :offerId ", Long.class).setParameter("offerId", o.getId()).getSingleResult();
-                stat.put(o, numberOfPurchasedOffer);
-            }
-
-            return stat;
-        } catch (PersistenceException e) {
-            throw new BadRequestException();
-        }
-    }
 
 
 
