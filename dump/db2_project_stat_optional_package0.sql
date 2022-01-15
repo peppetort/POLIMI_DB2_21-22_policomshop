ada9be0e42ea251ac2eca5f9ce1c9d0e233d54ba
Giuseppe Tortorelli <peppetort@gmail.com>
2022-01-15 12:42:47 +0100

    dump regenerated

---
+++ dump/db2_project_stat_optional_package0.sql
@@ -0,0 +1,55 @@
+-- MariaDB dump 10.19  Distrib 10.6.5-MariaDB, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: db2_project
+-- ------------------------------------------------------
+-- Server version	10.6.5-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `stat_optional_package`
+--
+
+DROP TABLE IF EXISTS `stat_optional_package`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stat_optional_package` (
+  `id_optional` int(11) NOT NULL,
+  `id_service_package` int(11) NOT NULL,
+  `num_purchases` int(11) NOT NULL,
+  UNIQUE KEY `stat_optional_package_id_optional_id_service_package_uindex` (`id_optional`,`id_service_package`),
+  KEY `stat_optional_package_service_package_id_fk` (`id_service_package`),
+  CONSTRAINT `stat_optional_package_optional_product_id_fk` FOREIGN KEY (`id_optional`) REFERENCES `optional_product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `stat_optional_package_service_package_id_fk` FOREIGN KEY (`id_service_package`) REFERENCES `service_package` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stat_optional_package`
+--
+
+LOCK TABLES `stat_optional_package` WRITE;
+/*!40000 ALTER TABLE `stat_optional_package` DISABLE KEYS */;
+INSERT INTO `stat_optional_package` VALUES (1,2,1),(2,2,1);
+/*!40000 ALTER TABLE `stat_optional_package` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-15 12:42:28
